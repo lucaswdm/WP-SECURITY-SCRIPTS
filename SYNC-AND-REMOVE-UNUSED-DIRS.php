981ff9d8b80c1530afecbc04515f7877af7f2034
lucaswdm <lucas@data2.com.br>
2023-01-12 10:39:28 -0300

    Update SYNC-AND-REMOVE-UNUSED-DIRS.php

--- SYNC-AND-REMOVE-UNUSED-DIRS.php
+++ SYNC-AND-REMOVE-UNUSED-DIRS.php
@@ -9,5 +9,24 @@ function RUN_AT_DIR($DIR) {
 }
 
 foreach(glob('/data/*/') as $DIR) {
+    $DOMAIN = basename($DIR);
+     if( !validateDomain($DOMAIN) ||  strtolower($DIR) != $DIR || !is_dir($DIR)) continue;
      RUN_AT_DIR($DIR);
+}
+
+
+
+
+
+
+
+
+
+function validateDomain($domain_name)
+{
+    if(filter_var($domain_name, FILTER_VALIDATE_IP)) return false;
+    if(strpos($domain_name, '.') === false) return false;
+    return (preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain_name) //valid chars check
+            && preg_match("/^.{1,253}$/", $domain_name) //overall length check
+            && preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain_name)   ); //length of each label
 }
